d3be1824329234e95227b02efe5fded6af4fa8c2
yassine <abidiyassine@outlook.com>
2025-04-17 17:40:13 +0100

    changes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,7 @@
-/*import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+// app/api/auth/[...nextauth]/route.ts
+import NextAuth from 'next-auth';
+import { authOptions } from './auth-options';
 
-export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: '/signin', // Custom sign-in page
-  },
-});*/
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
